173f5dcf407e3e30c918171c061cbf1bfabab7bb
Aman <54789531+eppisai@users.noreply.github.com>
2022-02-10 07:25:14 +0530

    Update solve_me.py

--- solve_me.py
+++ solve_me.py
@@ -76,28 +76,104 @@ $ python tasks.py report # Statistics
 $ python tasks.py runserver # Starts the tasks management server"""
         )
 
+    def __increment_priority(self, priority):
+        if priority in self.current_items:
+            self.__increment_priority(priority+1)
+        self.current_items[priority] = self.current_items[priority - 1]
+
     def add(self, args):
-        pass  # Use your existing implementation
+        priority = int(args[0])
+        if priority in self.current_items:
+            self.__increment_priority(priority + 1)
+        task = " ".join(args[1:])
+        self.current_items[priority] = task
+        self.write_current()
+        print(f'Added task: "{task}" with priority {priority}')
 
     def done(self, args):
-        pass  # Use your existing implementation
+        priority = int(args[0])
+        if priority in self.current_items:
+            self.completed_items.append(self.current_items[priority])
+            del self.current_items[priority]
+            print('Marked item as done.')
+        else:
+            print(
+                f"Error: no incomplete item with priority {priority} exists.")
+
+        self.write_completed()
+        self.write_current()
 
     def delete(self, args):
-        pass  # Use your existing implementation
+        priority = int(args[0])
+        if self.current_items.pop(priority, None):
+            self.write_current()
+            print(f"Deleted item with priority {priority}")
+        else:
+            print(
+                f"Error: item with priority {priority} does not exist. Nothing deleted."
+            )
 
     def ls(self):
-        pass  # Use your existing implementation
+        i = 1
+        for key, value in self.current_items.items():
+            print(f"{i}. {value} [{key}]")
+            i = i + 1
 
     def report(self):
-        pass  # Use your existing implementation
+        print(f"Pending : {len(self.current_items)}")
+        i = 1
+        for key, value in self.current_items.items():
+            print(f"{i}. {value} [{key}]")
+            i = i + 1
+        i = 1
+        print(f"\nCompleted : {len(self.completed_items)}")
+        for item in self.completed_items:
+            print(f"{i}. {item}")
+            i = i + 1
 
     def render_pending_tasks(self):
         # Complete this method to return all incomplete tasks as HTML
-        return "<h1> Show Incomplete Tasks Here </h1>"
+        self.read_current()
+        if len(self.current_items) == 0:
+            return "<h1> There are no pending tasks! </h1>"
+
+        text = "<h1> Incomplete Tasks : </h1> <h4> <ol>"
+        current_items_list = sorted(self.current_items.items(), key=lambda x:x[0])
+        for i in range(len(current_items_list)):
+            text += f"<li> {str(current_items_list[i][1])} [ {str(current_items_list[i][0])} ] </li> <br>"
+        
+        text += "</ol> </h4>"
+        return text
 
     def render_completed_tasks(self):
         # Complete this method to return all completed tasks as HTML
-        return "<h1> Show Completed Tasks Here </h1>"
+        self.read_completed()
+        if len(self.completed_items) == 0:
+            return "<h1> There are no completed tasks! </h1>"
+            
+        text = "<h1> Completed Tasks : </h1> <h4> <ol>"
+        for completed_item in self.completed_items:
+            text += f"<li> {completed_item} </li> <br>"
+
+        text += "</ol> </h4>"
+        return text
+
+
+class TasksServer(TasksCommand, BaseHTTPRequestHandler):
+    def do_GET(self):
+        task_command_object = TasksCommand()
+        if self.path == "/tasks":
+            content = task_command_object.render_pending_tasks()
+        elif self.path == "/completed":
+            content = task_command_object.render_completed_tasks()
+        else:
+            self.send_response(404)
+            self.end_headers()
+            return
+        self.send_response(200)
+        self.send_header("content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(content.encode())
 
 
 class TasksServer(TasksCommand, BaseHTTPRequestHandler):
